8468d9a92454fce7e75d7ca1c3ed7cb2872dc513
YJ <yujungpark@outlook.com>
2023-04-17 17:00:21 -0400

    Adding the First Modal

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+# from django.http import HttpResponse, HttpRequest
+# from django.http import HttpResponseBadRequest
+
 #from django.core import serializers
 
 # Create your views here.
@@ -26,13 +29,26 @@ def load_post_data_view(request, num_posts):
             'body' : obj.body,
             'liked': True if request.user in obj.liked.all() else False,
             'count': obj.like_count,
-            'author' : obj.author.user.username
-
+            'author' : obj.author.user.username           
         }
         data.append(item)
 
 
     return JsonResponse({'data': data[lower:upper], 'size': size})
+
+
+
+def like_unlike_post(request):
+    if request.is_ajax():
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
 
 
 def hello_world_view(request):
