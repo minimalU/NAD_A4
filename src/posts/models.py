c316bcb16260d39025273e116edf6f264ab3afbc
YJ <yujungpark@outlook.com>
2023-04-17 12:38:59 -0400

    Getting Posts Objects with Ajax

--- src/posts/models.py
+++ src/posts/models.py
@@ -12,6 +12,7 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
 
+
+    def __str__(self):
         return str(self.title)
