46664062fb7c9d45806803e1415b41c8b86fd1db
YJ <yujungpark@outlook.com>
2023-04-17 12:46:16 -0400

    Adding the Spinner

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -2,7 +2,7 @@ console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
-const spinnerBox = document.getElementById('spinner')
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET',
@@ -22,12 +22,18 @@ $.ajax({
     success: function(response){
         console.log(response)
         const data = response.data
-        console.log(data)       
-        data.forEach(el => {
+
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)       
+            data.forEach(el => {
             postsBox.innerHTML += `
                 ${el.title} - <b>${el.body}</b><br>
             `
         });
+
+        }, 100)
+        
 
     },
     error: function(error) {
